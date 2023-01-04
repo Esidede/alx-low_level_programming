864aab630ba6c55906e7b60c0c7a1b2b71f65c90
Esidede <audreymapfearon@gmail.com>
2023-01-04 05:53:31 -0800

    add

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,19 +8,22 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
-	int r;
+	int r, i;
 
-	while (*s)
+	for (r = 0; s[r] != '\0'; r++)
 	{
-		for (r = 0; accept[r] != '\0'; r++)
-			if (*s == accept[r])
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (s[r] == accept[i])
 			{
 				n++;
 				break;
 			}
-			if (accept[r + 1] == '\0' && s[r] != accept[r])
+			if (accept[i + 1] == '\0' && s[r] != accept[i])
+			{
 				return (n);
+			}
+		}
 	}
-	s++;
 	return (n);
 }
