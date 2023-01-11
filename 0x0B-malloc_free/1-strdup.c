2b2d03910f1a2fe04541c5b7f702182713c07fe8
Esidede <audreymapfearon@gmail.com>
2023-01-10 22:32:52 -0800

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,19 +1,19 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _strdup - returns a pointer to a newly alloctaed
- * space in memory which contains a copy of the string
- * passed.
- * @str: pointer to string being duplicated.
+ *_strdup - returns a pointer to a newly alloctaed
+ *space in memory which contains a copy of the string
+ *passed.
+ *@str: pointer to string being duplicated.
  *
- * Return: NULL if str is NULL.
- * pointer to duplicated string on success.
- * NULL if memory was insufficient.
- * */
+ *Return: NULL if str is NULL.
+ *pointer to duplicated string on success.
+ *NULL if memory was insufficient.
+ */
 char *_strdup(char *str)
 {
 	char *nstr;
-	unsigned int len. i;
+	unsigned int len, i;
 
 	/* check is str is null */
 	if (str == NULL)
